25d3ea38f73f420153a8cdba58a9754a63664020
hp <sultonovazizbek73@gmail.com>
2024-06-21 23:21:59 +0500

    started

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:fit_app/pages/starting_page/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home:  SplashPage(),
+    );
+  }
+}
+
