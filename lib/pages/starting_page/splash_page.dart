f38eb67e3f4cedfda77a76ec5d88a792542e3c18
hp <sultonovazizbek73@gmail.com>
2024-06-21 23:24:11 +0500

    app bar

--- lib/pages/starting_page/splash_page.dart
+++ lib/pages/starting_page/splash_page.dart
@@ -4,6 +4,8 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
